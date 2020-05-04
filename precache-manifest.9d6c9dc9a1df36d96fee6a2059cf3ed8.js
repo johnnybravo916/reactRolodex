8f21b977bb58db3cb6507795294669d06812c7e9
johnnybravo916 <johnong916@gmail.com>
2020-05-03 20:13:38 -0500

    Updates

---
+++ precache-manifest.9d6c9dc9a1df36d96fee6a2059cf3ed8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea91a54b82a533fdd601456ec7f8d200",
+    "url": "/reactRolodex/index.html"
+  },
+  {
+    "revision": "40436ae5051903532774",
+    "url": "/reactRolodex/static/css/main.fd6357d9.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/reactRolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactRolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40436ae5051903532774",
+    "url": "/reactRolodex/static/js/main.dc7c0098.chunk.js"
+  },
+  {
+    "revision": "0b80e75e4b389e57849d",
+    "url": "/reactRolodex/static/js/runtime-main.1d460d47.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/reactRolodex/precache-manifest.9d6c9dc9a1df36d96fee6a2059cf3ed8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactRolodex/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
